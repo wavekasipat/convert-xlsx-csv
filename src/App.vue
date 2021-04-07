cb5832951d9ffc91ca6a441cbf0110eff2ac520b
kasipat <kasipsho@metrosystems.co.th>
2021-04-07 18:22:24 +0700

    add Home.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <Home />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Home from "./components/Home.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Home,
   },
 };
 </script>
@@ -21,6 +20,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 40px;
+  margin-bottom: 40px;
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="home">
+    <el-card class="box-card">
+      <template #header>
+        <div class="card-header">
+          <h2>Convert XLSX to CSV</h2>
+        </div>
+      </template>
+
+      <el-upload
+        class="upload-demo"
+        action="https://jsonplaceholder.typicode.com/posts/"
+        :on-success="handleSuccess"
+        :on-remove="handleRemove"
+        :limit="1"
+        :file-list="fileList"
+        :multiple="false"
+      >
+        <el-button size="small" type="primary">Click to upload</el-button>
+        <template #tip>
+          <div class="el-upload__tip">xlsx file</div>
+        </template>
+      </el-upload>
+      <br />
+
+      <el-button type="success">Download CSV</el-button>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  props: {},
+  data() {
+    return {
+      fileList: [],
+    };
+  },
+  methods: {
+    handleRemove(file, fileList) {
+      console.log(file, fileList);
+    },
+    handleSuccess(response, file, fileList) {
+      console.log(response, file, fileList);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.box-card {
+  width: 480px;
+}
+</style>
